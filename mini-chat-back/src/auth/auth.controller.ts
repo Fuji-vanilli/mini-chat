7e3b166c4f274683a53c8c904ff729e3dfa4afb0
Fuji-Vanilli <ijufillivan@gmail.com>
2025-10-05 10:12:14 +0300

    update

--- mini-chat-back/src/auth/auth.controller.ts
+++ mini-chat-back/src/auth/auth.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService, type UserLoginBody } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private readonly authService: AuthService) {}
+
+    @Post('login')
+    login(@Body() userLoginBody: UserLoginBody) {
+        return this.authService.login(userLoginBody);
+    }
+}
--- mini-chat-back/src/auth/auth.service.ts
+++ mini-chat-back/src/auth/auth.service.ts
@@ -1,4 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import bcrypt from 'bcrypt';
+
+export type UserLoginBody = {
+    email: string;
+    password: string;
+}
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(private readonly prismaService: PrismaService) {}
+
+    login(userLoginBody: UserLoginBody) {
+        const existingUser = this.prismaService.user.findUnique({
+            where: { email: userLoginBody.email },
+        });
+
+        if (!existingUser) {
+            throw new Error('User not found !!!');
+        }
+
+        if (existingUser.password !== this.hashPassword(userLoginBody.password)) {
+            throw new Error('Error password !!!');
+        }
+        
+        return ;
+    }
+
+    hashPassword(password: string) {
+        return bcrypt.hash(password, 10);
+    }
+}
