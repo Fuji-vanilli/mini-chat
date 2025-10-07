5091f5020f521ed0f3e6ce52a663f5f79ab9ce77
Fuji-Vanilli <ijufillivan@gmail.com>
2025-10-07 09:21:07 +0300

    update

---
+++ mini-chat-front/app/auth.server.ts
@@ -0,0 +1,24 @@
+import z from "zod";
+import { getUserToken } from "./sessions.server";
+
+const getAuthenticatedUserSchema = z.object({
+  id: z.string(),
+  email: z.string(),
+  firstname: z.string(),
+  lastname: z.string(),
+});
+
+const getAuthenticatedUser = async (request: Request) => {
+    const response = await fetch("http://localhost:3000/auth/me", {
+        headers: {
+            Authorization: `Bearer ${await getUserToken(request)}`,
+        },
+    });
+
+    if (!response.ok) return null;
+
+    const data = await response.json();
+    const result = getAuthenticatedUserSchema.parse(data);
+
+    return result;
+}
